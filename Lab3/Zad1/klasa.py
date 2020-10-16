2bf7727f34d2a729d39a8cca796cd1c60c1c02c5
unknown <github@chadam.pl>
2020-10-16 13:23:50 +0200

    Lab 3 - Zadanie 1

---
+++ Lab3/Zad1/klasa.py
@@ -0,0 +1,36 @@
+class Klasa(object):
+    tab = []
+    _zmienna1 = None
+    __zmienna2 = None
+
+    def __init__(self, tablica, _zmienna1, __zmienna2):
+        print("Wywołano metodę '__init__()'")
+        self.tab = tablica
+        self._zmienna1 = _zmienna1
+        self.__zmienna2 = __zmienna2
+
+    def __del__(self):
+        print("Wywołano metodę '__del__()'")
+
+    def __str__(self):
+        return "Wywołano metodę '__str__()'"
+
+    def metodaInstancyjna(self):
+        print("Wywołano metodę 'metodaInstancyjna()'")
+        print("\tWartosc instancyjna tab    ->", self.tab)
+        print("\tWartosc klasowa tab    ->", tab)
+
+    @classmethod
+    def metodaKlasowa(cls):
+        print("Wywołano metodę 'metodaKlasowa()'")
+
+    @staticmethod
+    def metodaStatyczna():
+        print("Wywołano metodę 'metodaStatyczna()'")
+
+
+if __name__ == "__main__":
+    obiekt = Klasa([4, 5, 6], 10, 20)
+    print(obiekt.tab)
+    print(obiekt._zmienna1)
+    print(obiekt._Klasa__zmienna2)
