c1c74ea739d19b96447bbb8f22d7e649439c84bc
unknown <github@chadam.pl>
2020-11-07 18:45:11 +0100

    Lab 6 - Zadanie 2.3 Add comments

--- Lab6/Zad2/skrypt2.py
+++ Lab6/Zad2/skrypt2.py
@@ -1,5 +1,7 @@
 import sys
 
+# Jako argument przyjmuje nazwe pliku
+# Zwraca ilosc liczb parzystych w tym pliku
 def count_evens(filename):
     liczby = []
     with open(filename, "r") as plik:
@@ -7,6 +9,7 @@ def count_evens(filename):
             liczby += line.strip().split()
     return len([liczba for liczba in liczby if int(liczba) % 2 == 0 ])
 
+# One-liner do policzenia sumy dla wielu plikow
 print( sum( [count_evens(name) for name in sys.argv[1::]] ) )
 
 
