c8a994736b1d96b10fb5a29085aafbd39b162fdc
unknown <github@chadam.pl>
2020-11-23 11:16:30 +0100

    Lab 8 - zadanie 1

---
+++ Lab8/Zad1/script.js
@@ -0,0 +1,7 @@
+
+function klik() {
+    var liczba = document.getElementById("liczba").value;
+    var tekst = document.getElementById("tekst").value;
+    document.getElementById("wypisz_tekst").innerText = tekst;
+    document.getElementById("wypisz_liczbe").innerText = liczba;
+}
