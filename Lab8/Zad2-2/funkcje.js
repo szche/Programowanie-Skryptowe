76e98c4feaf7d73c9ec26537a6ac67faacb058ce
unknown <github@chadam.pl>
2020-11-26 13:55:06 +0100

    Lab 8 - Zad 2.2

---
+++ Lab8/Zad2-2/funkcje.js
@@ -0,0 +1,96 @@
+var poprzednie_linijki = [];
+
+function cyfry(napis) {
+    var cyfry = 0;
+    for(var i = 0; i < napis.length; i++) {
+        var litera = parseInt(napis[i]);
+        if(!isNaN(litera)) {
+            cyfry++;
+        }
+    }
+    return cyfry
+}
+
+function litery(napis) {
+    return napis.length - cyfry(napis)
+}
+
+function podziel(napis) {
+    zwroc = [];
+    for(var i = 0; i < napis.length; i++) {
+        wyraz = napis[i].split(' ');
+        for(var j =0; j < wyraz.length; j++) {
+            zwroc.push(wyraz[j]);
+        }
+    }
+    return zwroc;
+}
+
+function znajdz_wystapienia(napis, szukane) {
+    var wystapienia = 0;
+    for(var i =0; i<napis.length; i++) {
+        if (napis[i] == szukane) {
+            wystapienia++;
+        }
+    }
+    return wystapienia;
+}
+
+function liczba_w_linii(napis, liczba) {
+    var linie = [];
+    for(var i = 0; i<napis.length; i++) {
+        var linijka = napis[i];
+        var linijka_podzielona = linijka.split(" ");
+        for(var j = 0; j<linijka_podzielona.length; j++) {
+            var wyraz = linijka_podzielona[j];
+            if (liczba == wyraz) {
+                linie.push(i);
+            }
+        }
+    }
+    return linie;
+}
+
+function dzialaj(poprzednie, tekst) {
+    var wynik = new Map;
+    var wszystkie_linijki = []
+    if (poprzednie == true) {
+        wszystkie_linijki = poprzednie_linijki.concat(tekst.split("\n"));
+    }
+    else {
+        wszystkie_linijki = tekst.split("\n");
+    }
+    console.log("Dzialam na: ", wszystkie_linijki);
+    wszystkie_linijki_podzielone = podziel(wszystkie_linijki);
+    console.log("Linijki podzielone: ",wszystkie_linijki_podzielone);
+
+    textarea_podzielone = podziel( tekst.split("\n") );
+    console.log("Input podzielony: ", textarea_podzielone);
+
+    for(var i = 0; i< textarea_podzielone.length; i++) {
+        var wyraz = textarea_podzielone[i];
+        //Same cyfry
+        if(cyfry(wyraz) == wyraz.length) {
+            console.log("Cyfra \"",wyraz,"\" wystepuje w liniach: ", liczba_w_linii(wszystkie_linijki, wyraz));
+            wynik.set(wyraz, liczba_w_linii(wszystkie_linijki, wyraz));
+
+        }
+        //Same litery
+        if(litery(wyraz) == wyraz.length) {
+            console.log("Wystapienia wyrazy \"",wyraz,"\" : ", znajdz_wystapienia(wszystkie_linijki_podzielone, wyraz));
+            wynik.set(wyraz, znajdz_wystapienia(wszystkie_linijki_podzielone, wyraz));
+        }
+    }
+
+    poprzednie_linijki = poprzednie_linijki.concat(tekst.split('\n'));
+    return wynik;
+}
+
+function klik() {
+    var formularz = document.forms["wejscie"];
+    var tekst = formularz[0].value;
+    var poprzednie = formularz[1].checked;
+
+    var wynik = dzialaj(poprzednie, tekst);
+}
+
